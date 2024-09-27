d70852c50ee78d2cab4e9c2b40a3cd640bed3f5e
Biswajit Pradhan <143693333+biswapulan@users.noreply.github.com>
2024-09-28 00:14:38 +0530

    Update Login.js

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -25,7 +25,7 @@ function Login() {
       return handleError("email & password are required.");
     }
     try {
-      const url = "http://localhost:8080/auth/login";
+      const url = "https://saundarya-five.vercel.app/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
